33359fc1cc8b63fa4df9802ac24f84428dbdc7f3
Viktor Nemov <vik.nemov.vas@yandex.ru>
2022-11-02 11:30:57 +0300

    сделал burger-menu

--- src/widgets/burger-menu/BurgerMenu.vue
+++ src/widgets/burger-menu/BurgerMenu.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="burger">
-    <div class="burger__btn" @click="checkedBurger()"
-    :class="{'burger__btn--active': burgerCheck}">
+    <div class="burger__btn" @click="burgerCheck = !burgerCheck" :class="{'burger__btn--active': burgerCheck}">
       <span class="burger__line"></span>
     </div>
     <div class="burger__lang">Eng</div>
-    <div class="burger__nav nav" :class="{'is-visible': burgerCheck}">
-      <ul class="nav__list-link list-link">
-        <router-link class="list-link__item" tag="li" to="/">Парковка</router-link>
-        <router-link class="list-link__item" tag="li" to="/">Страховка</router-link>
-        <router-link class="list-link__item" tag="li" to="/">Бензин</router-link>
-        <router-link class="list-link__item" tag="li" to="/">Обслуживание</router-link>
-      </ul>
-      <ul class="nav__list-icon">
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+    <div class="burger__block" :class="{ 'is-visible': burgerCheck }">
+      <div class="burger__nav nav">
+        <ul class="nav__list-link list-link">
+          <router-link class="list-link__item" tag="li" to="/">Парковка</router-link>
+          <router-link class="list-link__item" tag="li" to="/">Страховка</router-link>
+          <router-link class="list-link__item" tag="li" to="/">Бензин</router-link>
+          <router-link class="list-link__item" tag="li" to="/">Обслуживание</router-link>
+        </ul>
+        <ul class="burger__list-icon list-icon">
+          <li class="list-icon__item"><a href="https://web.telegram.org/"><img
+                src="../../app/assets/img/burger/free-icon-telegram-3536661.png" alt="Telegram"></a></li>
+          <li class="list-icon__item"><a href="https://www.facebook.com/"><img
+                src="../../app/assets/img/burger/free-icon-facebook-174848.png" alt="Facebook"></a></li>
+          <li class="list-icon__item"><a href="https://www.instagram.com/"><img
+                src="../../app/assets/img/burger/free-icon-instagram-1409946.png" alt="Instagram"></a></li>
+        </ul>
+      </div>
+      <div class="burger__dark" @click="burgerCheck = !burgerCheck" v-show="burgerCheck"></div>
     </div>
 
   </div>
@@ -29,12 +34,6 @@ export default {
       burgerCheck: false,
     }
   },
-  methods: {
-    checkedBurger () {
-      this.burgerCheck = !this.burgerCheck;
-      console.log(this.burgerCheck);
-    }
-  }
 
 }
 </script>
